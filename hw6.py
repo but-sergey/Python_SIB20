e46bb2e33cd49f83a3172011e704ce17ef6458ec
but-sergey <but-sergey@bk.ru>
2022-07-09 14:24:04 +0300

    Task 1

--- hw6.py
+++ hw6.py
@@ -1,1 +1,100 @@
-#placeholder
+class Sheep:
+    sound = 'бе бе'
+
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+
+    def say(self):
+        print(self.sound)
+
+    def feeding(self):
+        print('Вы покормили', self.name)
+
+    def shearing(self):
+        print('Вы постригли', self.name)
+
+
+class Goose:
+    sound = 'га га'
+
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+
+    def say(self):
+        print(self.sound)
+
+    def feeding(self):
+        print('Вы покормили', self.name)
+
+    def collect_eggs(self):
+        print('Вы собрали яйца у ', self.name)
+
+
+class Chicken:
+    sound = 'ко ко'
+
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+
+    def say(self):
+        print(self.sound)
+
+    def feeding(self):
+        print('Вы покормили', self.name)
+
+    def collect_eggs(self):
+        print('Вы собрали яйца у ', self.name)
+
+
+class Duck:
+    sound = 'кря кря'
+
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+
+    def say(self):
+        print(self.sound)
+
+    def feeding(self):
+        print('Вы покормили', self.name)
+
+    def collect_eggs(self):
+        print('Вы собрали яйца у ', self.name)
+
+
+class Cow:
+    sound = 'му му'
+
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+
+    def say(self):
+        print(self.sound)
+
+    def feeding(self):
+        print('Вы покормили', self.name)
+
+    def milk(self):
+        print('Вы подоили', self.name)
+
+
+class Goat:
+    sound = 'бе бе'
+
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+
+    def say(self):
+        print(self.sound)
+
+    def feeding(self):
+        print('Вы покормили', self.name)
+
+    def milk(self):
+        print('Вы подоили', self.name)
